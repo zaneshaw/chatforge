da7b5d3c6f788fab6352dbd771f5b6a154f19e52
Zane Shaw <zane@squidee.dev>
2026-01-17 17:02:10 +1100

    add base layout and styling

---
+++ src/lib/components/Setting.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	type Props = {
+		children: any;
+		label: string;
+		isChild?: boolean;
+	};
+
+	let { children, label, isChild = false }: Props = $props();
+</script>
+
+{#if isChild}
+	<div class="between">
+		<span>{label}</span>
+		{@render children()}
+	</div>
+{:else}
+	<div class="box between p-2 [&>div]:ml-3">
+		<h2>{label}</h2>
+		{@render children()}
+	</div>
+{/if}
---
+++ src/lib/components/SettingsGroup.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	type Props = {
+		children: any;
+		label: string;
+	};
+
+	let { children, label }: Props = $props();
+</script>
+
+<div class="box flex flex-col gap-1 p-2 [&>div]:ml-3">
+	<h2>{label}</h2>
+	{@render children()}
+</div>
