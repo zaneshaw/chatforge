d39336c3a012283260f59634cba81fcc81f80f61
Zane Shaw <zane@squidee.dev>
2026-01-17 17:02:10 +1100

    add base layout and styling

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
