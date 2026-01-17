35d0246abe1a953ae32db1d9b718a3e085deb640
Zane Shaw <zane@squidee.dev>
2026-01-17 17:51:33 +1100

    add preferences modal skeleton

--- src/lib/components/Setting.svelte
+++ src/lib/components/Setting.svelte
@@ -3,18 +3,20 @@
 		children: any;
 		label: string;
 		isChild?: boolean;
+		minimal?: boolean;
+		class?: string;
 	};
 
-	let { children, label, isChild = false }: Props = $props();
+	let { children, label, isChild = false, minimal = false, class: _class = "" }: Props = $props();
 </script>
 
 {#if isChild}
-	<div class="between">
+	<div class="{minimal ? "" : "between"} {_class}">
 		<span>{label}</span>
 		{@render children()}
 	</div>
 {:else}
-	<div class="box between p-2 [&>div]:ml-3">
+	<div class="{minimal ? "" : "box between p-2"} {_class}">
 		<h2>{label}</h2>
 		{@render children()}
 	</div>
