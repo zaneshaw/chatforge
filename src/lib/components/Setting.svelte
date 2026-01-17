2d955d9ce7c2cd2113fd9f138e6a7f7fde17717c
Zane Shaw <zane@squidee.dev>
2026-01-18 01:55:09 +1100

    add settings

--- src/lib/components/Setting.svelte
+++ src/lib/components/Setting.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+	import { settingsStorage } from "../stores/settingsStorage";
+
 	type Props = {
 		children: any;
 		label: string;
+		key?: string;
+		value?: any;
 		isChild?: boolean;
 		minimal?: boolean;
 		class?: string;
 	};
 
-	let { children, label, isChild = false, minimal = false, class: _class = "" }: Props = $props();
+	let { children, label, key, value = $bindable(), isChild = false, minimal = false, class: _class = "" }: Props = $props();
+
+	onMount(() => {
+		if (key != undefined && key in $settingsStorage) {
+			value = $settingsStorage[key];
+		}
+	});
+
+	$effect(() => {
+		if (key != undefined) {
+			$settingsStorage[key] = value;
+		}
+	});
 </script>
 
 {#if isChild}
-	<div class="{minimal ? "" : "between"} {_class}">
+	<div class="{minimal ? '' : 'between'} {_class}">
 		<span>{label}</span>
 		{@render children()}
 	</div>
 {:else}
-	<div class="{minimal ? "" : "box between p-2"} {_class}">
+	<div class="{minimal ? '' : 'box between p-2'} {_class}">
 		<h2>{label}</h2>
 		{@render children()}
 	</div>
