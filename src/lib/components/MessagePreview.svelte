f483fd79b2acc742cd13b6dfe629c6af39f56477
Zane Shaw <zane@squidee.dev>
2026-01-18 15:29:33 +1100

    use contenteditable spans instead of inputs

--- src/lib/components/MessagePreview.svelte
+++ src/lib/components/MessagePreview.svelte
@@ -4,58 +4,40 @@
 	const USERNAME_PLACEHOLDER = "username";
 	const MESSAGE_PLACEHOLDER = "click to edit";
 
-	let usernameValue = $state("");
-	let usernameWidth = $derived(getTextWidth(usernameValue || USERNAME_PLACEHOLDER, 14, 700));
-
-	let messageValue = $state("");
-	let messageWidth = $derived(getTextWidth(messageValue || MESSAGE_PLACEHOLDER, 14, 500));
-
-	function getTextWidth(text: string, size: number = 14, weight: number = 500) {
-		let width: number;
-
-		const temp = document.createElement("span");
-		temp.style.position = "absolute";
-		temp.style.whiteSpace = "pre";
-		temp.style.fontSize = `${size}px`;
-		temp.style.fontWeight = `${weight}`;
-		temp.innerText = text;
-
-		document.body.appendChild(temp);
-		width = temp.offsetWidth;
-		document.body.removeChild(temp);
-
-		return width;
-	}
+	let usernameValue = $state(USERNAME_PLACEHOLDER);
+	let messageValue = $state(MESSAGE_PLACEHOLDER);
 </script>
 
 <div class="flex size-full items-center justify-center text-sm">
 	{#await document.fonts.ready}
 		<span class="text-zinc-600">loading font...</span>
 	{:then}
-		<Setting label="" key="username.text" bind:value={usernameValue} minimal>
-			<input
-				type="text"
-				bind:value={usernameValue}
-				onfocus={(e) => (e.target as HTMLInputElement).select()}
-				placeholder={USERNAME_PLACEHOLDER}
-				autocomplete="off"
-				spellcheck="false"
-				class="rounded-xs p-0 font-bold outline-0 outline-zinc-700 hover:outline-1 focus:outline-1 focus:outline-zinc-600"
-				style="width: {usernameWidth}px;"
-			/>
-		</Setting>
-		<span class="mr-1">:</span>
-		<Setting label="" key="message.text" bind:value={messageValue} minimal>
-			<input
-				type="text"
-				bind:value={messageValue}
-				onfocus={(e) => (e.target as HTMLInputElement).select()}
-				placeholder={MESSAGE_PLACEHOLDER}
-				autocomplete="off"
-				spellcheck="false"
-				class="rounded-xs p-0 outline-0 outline-zinc-700 hover:outline-1 focus:outline-1 focus:outline-zinc-600"
-				style="width: {messageWidth}px;"
-			/>
-		</Setting>
+		<div class="flex max-w-75">
+			<Setting label="" key="username.text" bind:value={usernameValue} minimal>
+				<span
+					bind:textContent={usernameValue}
+					contenteditable="true"
+					onfocus={(e) => (e.target as HTMLInputElement).select()}
+					onblur={() => {
+						if (usernameValue.length == 0) usernameValue = USERNAME_PLACEHOLDER;
+					}}
+					spellcheck="false"
+					class="inline-block min-w-0.75 rounded-xs p-0 font-bold outline-0 outline-zinc-700 hover:outline-1 focus:outline-1 focus:outline-zinc-600"
+				></span>
+			</Setting>
+			<span class="mr-1">:</span>
+			<Setting label="" key="message.text" bind:value={messageValue} minimal>
+				<span
+					bind:textContent={messageValue}
+					contenteditable="true"
+					onfocus={(e) => (e.target as HTMLInputElement).select()}
+					onblur={() => {
+						if (messageValue.length == 0) messageValue = MESSAGE_PLACEHOLDER;
+					}}
+					spellcheck="false"
+					class="inline-block min-w-0.75 rounded-xs p-0 wrap-break-word break-all outline-0 outline-zinc-700 hover:outline-1 focus:outline-1 focus:outline-zinc-600"
+				></span>
+			</Setting>
+		</div>
 	{/await}
 </div>
