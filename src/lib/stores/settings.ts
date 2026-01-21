0b1777e573615d941ae655608afcc9461295093b
Zane Shaw <zane@squidee.dev>
2026-01-21 14:54:34 +1100

    improve settings handling

--- src/lib/stores/settings.ts
+++ src/lib/stores/settings.ts
@@ -1,19 +1,19 @@
 import { flatten, unflatten } from "flat";
 import { writable, type Writable } from "svelte/store";
 
-export const settingsStorage: Writable<{ [key: string]: any }> = writable(getSettingsFromStorage());
+export const settings: Writable<{ [key: string]: any }> = writable(getSettingsFromStorage());
 
 function getSettingsFromStorage() {
 	const storage = localStorage.getItem("settings");
 
 	if (storage) {
-		return flatten(JSON.parse(storage)) as any;
+		return JSON.parse(storage);
 	}
 
 	localStorage.setItem("settings", "{}");
 	return {};
 }
 
-settingsStorage.subscribe((value: any) => {
-	localStorage.setItem("settings", JSON.stringify(unflatten(value)));
+settings.subscribe((value: any) => {
+	localStorage.setItem("settings", JSON.stringify(value));
 });
