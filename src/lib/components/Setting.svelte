261335c4666d99e0c3cf013b7ea0e2be5870ed53
Zane Shaw <zane@squidee.dev>
2026-01-21 14:54:34 +1100

    improve settings handling

--- src/lib/components/Setting.svelte
+++ src/lib/components/Setting.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { settingsStorage } from "../stores/settingsStorage";
+	import { settings } from "../stores/settings";
+	import { flatten, unflatten } from "flat";
+	import _ from "lodash";
 
 	type Props = {
 		children: any;
@@ -15,31 +17,38 @@
 	let { children, label, key, value = $bindable(), isChild = false, minimal = false, class: _class = "" }: Props = $props();
 
 	onMount(() => {
-		if (key != undefined && key in $settingsStorage) {
-			value = $settingsStorage[key];
+		if (key != undefined) {
+			const flattened = flatten($settings) as any;
+			if (key in flattened) {
+				value = flattened[key];
+			}
 		}
 	});
 
 	$effect(() => {
 		if (key != undefined) {
-			$settingsStorage[key] = value;
+			settings.update((state) =>
+				_.merge(
+					state,
+					unflatten({
+						[key]: value,
+					}),
+				),
+			);
 		}
 	});
 </script>
 
 {#if minimal}
 	{@render children?.()}
+{:else if isChild}
+	<div class="between h-6 {_class}">
+		<span>{label}</span>
+		{@render children?.()}
+	</div>
 {:else}
-	{#if isChild}
-		<div class="between h-6 {_class}">
-			<span>{label}</span>
-			{@render children?.()}
-		</div>
-	{:else}
-		<div class="box between h-10 p-2 {_class}">
-			<h2>{label}</h2>
-			{@render children?.()}
-		</div>
-	{/if}
+	<div class="box between h-10 p-2 {_class}">
+		<h2>{label}</h2>
+		{@render children?.()}
+	</div>
 {/if}
-
