1743d5bc948756c05d561398cc694932ee9262d7
Zane Shaw <zane@squidee.dev>
2026-01-22 23:33:03 +1100

    add twitch badges

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
+import { loadBadges } from "./lib/stores/badges";
 
 const app = mount(App, {
 	target: document.body,
 });
+
+loadBadges();
 
 export default app;
