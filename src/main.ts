470e63a49b523d1059aa2054213d7f489ad40b0f
Zane Shaw <zane@squidee.dev>
2026-01-23 19:07:47 +1100

    remove tauri log plugin

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 import { loadBadges } from "./lib/stores/badges";
-import { fetch } from "@tauri-apps/plugin-http";
+import { loading } from "./lib/stores/loading.svelte";
 
 const app = mount(App, {
 	target: document.body,
 });
 
 await loadBadges();
+loading.state = false;
 
 export default app;
