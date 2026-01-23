d37deeaefbff54a81a905499269e73a6d695db6e
Zane Shaw <zane@squidee.dev>
2026-01-23 17:47:26 +1100

    add ffz, bttv, and 7tv badges
    todo: ffz badge colour

--- src/lib/stores/badges.ts
+++ src/lib/stores/badges.ts
@@ -2,10 +2,11 @@ import { writeFile, mkdir, BaseDirectory } from "@tauri-apps/plugin-fs";
 import { join, dirname, appCacheDir } from "@tauri-apps/api/path";
 import { convertFileSrc } from "@tauri-apps/api/core";
 import { load } from "@tauri-apps/plugin-store";
-import { loading } from "./loading.svelte";
-
 import { fetch } from "@tauri-apps/plugin-http";
 import { parse } from "node-html-parser";
+import { loading } from "./loading.svelte";
+import { settings } from "./settings";
+import { get } from "svelte/store";
 
 export type Badge = {
 	id: string;
@@ -15,10 +16,10 @@ export type Badge = {
 	ts: number;
 };
 
-const checkBadges = false; // temp
-const cacheLifetime = 24 * 60 * 60 * 1000;
+const checkCacheInterval = 24 * 60 * 60 * 1000; // check cache after 24 hours
+const cacheLifetime = 28 * 24 * 60 * 60 * 1000; // expire after 28 days
 
-const badgeCacheStore = await load("badge_cache.json", {
+const badgeCache = await load("badge_cache.json", {
 	defaults: {} as {
 		[id: string]: Badge;
 	},
@@ -44,70 +45,164 @@ export function getBadgeUrl(id: string, quality: number) {
 }
 
 export async function loadBadges() {
-	badges.push(...(await loadTwitchBadges()));
+	const lastBadgeCheck = get(settings).last_badge_check;
+	const now = Date.now();
 
-	loading.progress = 1;
-	loading.max = 1;
-	loading.label = "Finished Loading"
-	loading.showProgress = false;
+	if (!lastBadgeCheck || now >= lastBadgeCheck + checkCacheInterval) {
+		await loadTwitchBadges();
+		await loadFFZBadges();
+		await loadBTTVBadges();
+		await load7TVBadges();
 
-	await new Promise(r => setTimeout(r, 500));
+		settings.update((state) => {
+			state.last_badge_check = now;
+			return state;
+		});
+
+		loading.progress = 1;
+		loading.max = 1;
+		loading.label = "Done!";
+		loading.showProgressText = false;
+	}
+
+	const cached = (await badgeCache.values()) as Badge[];
+	badges.push(...cached);
+
+	await new Promise((r) => setTimeout(r, 500));
 
 	loading.state = false;
 }
 
+export function clearBadgeCache() {
+	badgeCache.clear();
+	badgeCache.save();
+
+	settings.update((state) => {
+		state.last_badge_check = 0;
+		return state;
+	});
+
+	location.reload();
+}
+
+async function tryWriteToCache(id: string, name: string, provider: string, url: string) {
+	const cached = await badgeCache.get<Badge>(id);
+	const now = Date.now();
+
+	if (!cached || now >= cached.ts + cacheLifetime) {
+		const badgeRes = await fetch(url);
+		const badgeBytes = new Uint8Array(await badgeRes.arrayBuffer());
+
+		// todo: better image type detection
+		let fileExtension = url.split(".").at(-1) || "png";
+		if (fileExtension.length > 4) fileExtension = "png";
+
+		const badgePath = await join("cache", "badges", provider, `${id}.${fileExtension}`);
+
+		await mkdir(await dirname(badgePath), { recursive: true, baseDir: BaseDirectory.AppCache });
+		await writeFile(badgePath, badgeBytes, { baseDir: BaseDirectory.AppCache });
+
+		await badgeCache.set(id, {
+			id: id,
+			name: name,
+			provider: provider,
+			path: await join(await appCacheDir(), badgePath),
+			ts: now,
+		} as Badge);
+	}
+}
+
 // todo: concurrency
-async function loadTwitchBadges(): Promise<Badge[]> {
-	if (checkBadges) {
-		loading.label = "Getting list of Twitch badges...";
+async function loadTwitchBadges() {
+	loading.label = "Getting list of Twitch badges...";
 
-		const res = await fetch("https://www.streamdatabase.com/twitch/global-badges?sort_by=set_id&sort_direction=ascending", {
-			method: "GET",
-		});
-		const root = parse(await res.text());
+	const res = await fetch("https://www.streamdatabase.com/twitch/global-badges?sort_by=set_id&sort_direction=ascending", { method: "GET" });
+	const root = parse(await res.text());
 
-		const column = root.querySelector("div.grow.flex.m-auto.w-full > div.flex.flex-col");
-		const badgeContainers = column?.querySelectorAll("a.relative.bg-neutral-900.rounded.font-bold.uppercase");
-		if (badgeContainers) {
-			loading.progress = 0;
-			loading.max = badgeContainers.length;
-			loading.label = "Caching Twitch badges...";
-			loading.showProgress = true;
+	const column = root.querySelector("div.grow.flex.m-auto.w-full > div.flex.flex-col");
+	const badgeContainers = column?.querySelectorAll("a.relative.bg-neutral-900.rounded.font-bold.uppercase");
+	if (badgeContainers) {
+		loading.progress = 0;
+		loading.max = badgeContainers.length;
+		loading.label = "Caching Twitch badges...";
+		loading.showProgressText = true;
 
-			const appCache = await appCacheDir();
+		for await (const badgeContainer of badgeContainers) {
+			const imageElement = badgeContainer.querySelector("img");
+			if (imageElement) {
+				const src = imageElement.getAttribute("src");
+				if (src) {
+					const badgeId = src.split("/").at(-2) as string;
 
-			for await (const [i, badgeContainer] of badgeContainers.entries()) {
-				const imageElement = badgeContainer.querySelector("img");
-				if (imageElement) {
-					const src = imageElement.getAttribute("src");
-					if (src) {
-						const badgeId = src.split("/").at(-2) as string;
-						const badgeCache = await badgeCacheStore.get<Badge>(badgeId);
-						const now = Date.now();
-
-						if (!badgeCache || now >= badgeCache.ts + cacheLifetime) {
-							const badgeRes = await fetch(src);
-							const badgeBytes = new Uint8Array(await badgeRes.arrayBuffer());
-							const badgePath = await join("cache", "badges", "twitch", `${badgeId}.png`);
-
-							await mkdir(await dirname(badgePath), { recursive: true, baseDir: BaseDirectory.AppCache });
-							await writeFile(badgePath, badgeBytes, { baseDir: BaseDirectory.AppCache });
-
-							await badgeCacheStore.set(badgeId, {
-								id: badgeId,
-								name: "aga",
-								provider: "twitch",
-								path: await join(appCache, badgePath),
-								ts: now,
-							} as Badge);
-						}
-					}
+					await tryWriteToCache(badgeId, "aga", "twitch", src);
 				}
-
-				loading.progress++;
 			}
+
+			loading.progress++;
 		}
 	}
+}
 
-	return await badgeCacheStore.values();
+async function loadFFZBadges() {
+	loading.label = "Getting list of FFZ badges...";
+
+	const res = await fetch("https://api.frankerfacez.com/v1/_badges", { method: "GET" });
+	const data = await res.json();
+
+	loading.progress = 0;
+	loading.max = data.badges.length;
+	loading.label = "Caching FFZ badges...";
+	loading.showProgressText = true;
+
+	for await (const badge of data.badges) {
+		await tryWriteToCache(`ffz_${badge.id}`, badge.title, "ffz", Object.values(badge.urls).at(-1) as string);
+
+		loading.progress++;
+	}
+}
+
+async function loadBTTVBadges() {
+	loading.label = "Getting list of BTTV badges...";
+
+	const res = await fetch("https://api.betterttv.net/3/cached/badges/twitch", { method: "GET" });
+	const users = await res.json();
+
+	const badgesStringified = new Set(users.map((user: any) => JSON.stringify(user.badge)));
+	const badges = Array.from(badgesStringified).map((x: any) => JSON.parse(x));
+	const badgesSorted = badges.sort((a, b) => a.type - b.type);
+
+	loading.progress = 0;
+	loading.max = badgesSorted.length;
+	loading.label = "Caching BTTV badges...";
+	loading.showProgressText = true;
+
+	for await (const badge of badgesSorted) {
+		await tryWriteToCache(`bttv_${badge.type}`, badge.description, "bttv", badge.svg);
+
+		loading.progress++;
+	}
+}
+
+async function load7TVBadges() {
+	loading.label = "Getting list of 7TV badges...";
+
+	const res = await fetch("https://7tv.io/v3/gql", {
+		method: "POST",
+		body: JSON.stringify({
+			query: "query Cosmetics { cosmetics { badges { id tooltip host { url } } } }",
+		}),
+	});
+	const data = await res.json();
+	const badges = data.data.cosmetics.badges;
+
+	loading.progress = 0;
+	loading.max = badges.length;
+	loading.label = "Caching 7TV badges...";
+	loading.showProgressText = true;
+
+	for await (const badge of badges) {
+		await tryWriteToCache(`7tv_${badge.id}`, badge.tooltip, "7tv", `https://${badge.host.url}/4x.webp`);
+
+		loading.progress++;
+	}
 }
