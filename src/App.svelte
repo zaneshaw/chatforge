69961baa2964afa30dd904bc1852482be50d11fd
Zane Shaw <zane@squidee.dev>
2026-02-04 11:38:28 +1100

    Revert "add base layout and styling"
    This reverts commit da7b5d3c6f788fab6352dbd771f5b6a154f19e52.

--- src/App.svelte
+++ src/App.svelte
@@ -1,57 +1,11 @@
 <script lang="ts">
-	import Setting from "./lib/components/Setting.svelte";
-	import SettingsGroup from "./lib/components/SettingsGroup.svelte";
-	import SettingsRow from "./lib/components/SettingsRow.svelte";
 </script>
 
-<main class="flex h-full grow flex-col justify-between">
-	<div class="flex flex-col gap-2">
-		<button class="btn ml-auto">Preferences</button>
-		<SettingsGroup label="Background">
-			<Setting label="Colour" isChild>
-				<input type="color" name="" id="" />
-			</Setting>
-			<Setting label="Opacity" isChild>
-				<input type="range" name="" id="" />
-			</Setting>
-		</SettingsGroup>
-		<SettingsGroup label="Outline">
-			<Setting label="Colour" isChild>
-				<input type="color" name="" id="" />
-			</Setting>
-			<Setting label="Opacity" isChild>
-				<input type="range" name="" id="" />
-			</Setting>
-		</SettingsGroup>
-		<SettingsRow>
-			<Setting label="First Message">
-				<input type="checkbox" name="" id="" />
-			</Setting>
-			<Setting label="Removed Message">
-				<input type="checkbox" name="" id="" />
-			</Setting>
-		</SettingsRow>
-		<Setting label="Emotes">
-			<button class="btn">Configure Emotes</button>
-		</Setting>
+<main class="grow flex justify-between h-full flex-col">
+	<div>
+		<h1>ChatForge</h1>
+		<p>yo</p>
 	</div>
-	<hr />
-	<div class="flex flex-col gap-2">
-		<div class="box relative h-62.5 w-full">
-			<button class="btn absolute top-1.5 left-1.5">Presets</button>
-			<label class="btn absolute top-1.5 right-1.5 flex items-center gap-1">
-				<input type="checkbox" name="" id="" />
-				Preview Background
-			</label>
-			<div class="flex size-full items-center justify-center text-sm">
-				<span class="font-bold">squidee_</span><span>: yo</span>
-			</div>
-		</div>
-		<div class="ml-auto flex">
-			<button class="btn rounded-r-none!">Quick Export</button>
-			<button class="btn rounded-l-none!">Export...</button>
-		</div>
-	</div>
+	<div class="h-62.5 w-full bg-twitch-background-light"></div>
 </main>
-
-<div class="bg-twitch-background-light w-full border-t border-zinc-800 px-3 py-0.5 text-sm font-light text-zinc-400">Hover over something for help...</div>
+<div class="w-full bg-twitch-background-light px-2 text-sm text-neutral-400 font-light py-0.5">hover an element for help</div>
