b2adb7246af71d360a53fbc3f4536f880c6d9a21
Zane Shaw <zane@squidee.dev>
2026-02-04 11:38:28 +1100

    Revert "unflatten settings in local storage"
    This reverts commit a13a1f1b70504a843df775b1f78b7b67b6cc78fc.

--- src/lib/stores/settingsStorage.ts
+++ src/lib/stores/settingsStorage.ts
@@ -1,4 +1,3 @@
-import { flatten, unflatten } from "flat";
 import { writable, type Writable } from "svelte/store";
 
 export const settingsStorage: Writable<{ [key: string]: any }> = writable(getSettingsFromStorage());
@@ -7,7 +6,7 @@ function getSettingsFromStorage() {
 	const storage = localStorage.getItem("settings");
 
 	if (storage) {
-		return flatten(JSON.parse(storage)) as any;
+		return JSON.parse(storage);
 	}
 
 	localStorage.setItem("settings", "{}");
@@ -15,5 +14,5 @@ function getSettingsFromStorage() {
 }
 
 settingsStorage.subscribe((value: any) => {
-	localStorage.setItem("settings", JSON.stringify(unflatten(value)));
+	localStorage.setItem("settings", JSON.stringify(value));
 });
