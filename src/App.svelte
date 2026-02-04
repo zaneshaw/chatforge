133287fef6aed0ca2b541049f3faaa1a5aafa23b
Zane Shaw <zane@squidee.dev>
2026-02-04 11:38:28 +1100

    Revert "tweak settings layout"
    This reverts commit 1e951ae7e5328c6a5c745977d5e55e11cdb1874e.

--- src/App.svelte
+++ src/App.svelte
@@ -9,65 +9,42 @@
 
 <main class="flex h-full grow flex-col justify-between">
 	<div class="flex flex-col gap-2">
-		<div class="between flex">
-			<div class="grow basis-0">
-				<button class="btn">Presets</button>
-			</div>
-			<div class="flex gap-2">
-				<img src="/garf.png" alt="" class="h-5" />
-				<h1>ChatForge</h1>
-				<img src="/garf.png" alt="" class="h-5" />
-			</div>
-			<div class="flex grow basis-0 justify-end">
-				<button class="btn" onclick={preferencesModal.open}>Preferences</button>
-			</div>
-		</div>
-		<SettingsRow>
-			<Setting label="Name Color">
-				<input type="color" name="" id="" class="w-9" />
+		<button class="btn ml-auto" onclick={preferencesModal.open}>Preferences</button>
+		<SettingsGroup label="Background">
+			<Setting label="Colour" isChild>
+				<input type="color" name="" id="" />
 			</Setting>
-			<Setting label="Message Color">
-				<input type="color" name="" id="" class="w-9" />
+			<Setting label="Opacity" isChild>
+				<input type="range" name="" id="" />
+			</Setting>
+		</SettingsGroup>
+		<SettingsGroup label="Outline">
+			<Setting label="Colour" isChild>
+				<input type="color" name="" id="" />
+			</Setting>
+			<Setting label="Opacity" isChild>
+				<input type="range" name="" id="" />
+			</Setting>
+		</SettingsGroup>
+		<SettingsRow>
+			<Setting label="First Message">
+				<input type="checkbox" name="" id="" />
+			</Setting>
+			<Setting label="Removed Message">
+				<input type="checkbox" name="" id="" />
 			</Setting>
 		</SettingsRow>
 		<Setting label="Emotes">
 			<button class="btn">Configure Emotes</button>
 		</Setting>
-		<SettingsRow>
-			<SettingsGroup label="Background">
-				<Setting label="Colour" isChild>
-					<input type="color" name="" id="" class="w-9" />
-				</Setting>
-				<Setting label="Opacity" isChild>
-					<input type="number" name="" id="" value="1" min="0" max="1" step="0.1" class="w-9" />
-				</Setting>
-			</SettingsGroup>
-			<SettingsGroup label="Outline">
-				<Setting label="Colour" isChild>
-					<input type="color" name="" id="" class="w-9" />
-				</Setting>
-				<Setting label="Thickness" isChild>
-					<input type="number" name="" id="" value="0" min="0" max="5" class="w-9" />
-				</Setting>
-			</SettingsGroup>
-		</SettingsRow>
-		<Setting label="Message Type">
-			<div class="ml-auto flex">
-				<button class="btn rounded-r-none! bg-zinc-800!">Default</button>
-				<button class="btn rounded-none! text-zinc-400">First</button>
-				<button class="btn rounded-l-none! text-zinc-400">Deleted</button>
-			</div>
-		</Setting>
-		<Setting label="Text Wrapping (force to Twitch's width)">
-			<input type="checkbox" name="" id="" />
-		</Setting>
 	</div>
 	<hr />
 	<div class="flex flex-col gap-2">
-		<div class="box relative h-62.5 w-full overflow-hidden">
-			<label class="btn absolute top-0 right-0 flex items-center gap-1.5 rounded-none! rounded-bl-xs!">
+		<div class="box relative h-62.5 w-full">
+			<button class="btn absolute top-1.5 left-1.5">Presets</button>
+			<label class="btn absolute top-1.5 right-1.5 flex items-center gap-1">
 				<input type="checkbox" name="" id="" />
-				Background Preview
+				Preview Background
 			</label>
 			<div class="flex size-full items-center justify-center text-sm">
 				<span class="font-bold">squidee_</span><span>: yo</span>
@@ -80,21 +57,10 @@
 	</div>
 </main>
 
-<div class="bg-twitch-background-light w-full border-t border-zinc-800 px-3 py-0.5 font-light text-zinc-500">Hover over something for help...</div>
+<div class="bg-twitch-background-light w-full border-t border-zinc-800 px-3 py-0.5 font-light text-zinc-400">Hover over something for help...</div>
 
 <Modal bind:this={preferencesModal} class="flex flex-col gap-2">
 	<h1>Preferences</h1>
-	<Setting label="Always On Top" minimal class="between">
-		<input type="checkbox" name="" id="" />
-	</Setting>
-	<Setting label="Emote Cache" isChild>
-		<button class="btn">Reset</button>
-	</Setting>
-	<Setting label="Badge Cache" isChild>
-		<button class="btn">Reset</button>
-	</Setting>
-	<br />
-	<Setting label="Factory Reset" isChild>
-		<button class="btn">Reset and Exit</button>
-	</Setting>
+	<Setting label="setting1" minimal class="between">input1</Setting>
+	<Setting label="setting2" minimal class="between">input2</Setting>
 </Modal>
--- src/lib/components/SettingsGroup.svelte
+++ src/lib/components/SettingsGroup.svelte
@@ -7,7 +7,7 @@
 	let { children, label }: Props = $props();
 </script>
 
-<div class="box flex flex-col gap-1 p-2 [&>div]:ml-1.5">
+<div class="box flex flex-col gap-1 p-2 [&>div]:ml-3">
 	<h2>{label}</h2>
 	{@render children()}
 </div>
