5d7ea96cc2e9979116bfaa5c1f9a9d6819100a8e
Zane Shaw <zane@squidee.dev>
2026-02-04 11:38:28 +1100

    Revert "add settings"
    This reverts commit 9c2d80d3a478502880c5babb0ad53da12d14c94f.

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { getCurrentWindow } from "@tauri-apps/api/window";
 	import { SettingsIcon } from "@lucide/svelte";
 	import Modal from "./lib/components/Modal.svelte";
 	import Setting from "./lib/components/Setting.svelte";
@@ -9,20 +8,10 @@
 
 	// svelte-ignore non_reactive_update
 	let preferencesModal: Modal;
-
-	let nameColourValue: string = $state("#ffffff");
-	let messageColourValue: string = $state("#ffffff");
-	let backgroundColourValue: string = $state("#18181b");
-	let backgroundOpacityValue: number = $state(1);
-	let outlineColourValue: string = $state("#000000");
-	let outlineThicknessValue: number = $state(0);
-	let messageTypeValue: string = $state("default");
-	let wrapValue: boolean = $state(false);
-	let alwaysOnTopValue: boolean = $state(true);
-	let backgorundPreviewValue: boolean = $state(false);
+	let messageType: string = $state("");
 
 	$effect(() => {
-		getCurrentWindow().setAlwaysOnTop(alwaysOnTopValue);
+		console.log(messageType);
 	});
 </script>
 
@@ -44,11 +33,11 @@
 			</div>
 		</div>
 		<SettingsRow>
-			<Setting label="Name Colour" key="name.colour" bind:value={nameColourValue}>
-				<input type="color" bind:value={nameColourValue} class="w-9" />
+			<Setting label="Name Colour">
+				<input type="color" name="" id="" class="w-9" />
 			</Setting>
-			<Setting label="Message Colour" key="message.colour" bind:value={messageColourValue}>
-				<input type="color" bind:value={messageColourValue} class="w-9" />
+			<Setting label="Message Colour">
+				<input type="color" name="" id="" class="w-9" />
 			</Setting>
 		</SettingsRow>
 		<Setting label="Emotes">
@@ -56,25 +45,25 @@
 		</Setting>
 		<SettingsRow>
 			<SettingsGroup label="Background">
-				<Setting label="Colour" key="bg.colour" bind:value={backgroundColourValue} isChild>
-					<input type="color" bind:value={backgroundColourValue} class="w-9" />
+				<Setting label="Colour" isChild>
+					<input type="color" name="" id="" class="w-9" />
 				</Setting>
-				<Setting label="Opacity" key="bg.opacity" bind:value={backgroundOpacityValue} isChild>
-					<input type="number" bind:value={backgroundOpacityValue} min="0" max="1" step="0.1" class="w-9" />
+				<Setting label="Opacity" isChild>
+					<input type="number" name="" id="" value="1" min="0" max="1" step="0.1" class="w-9" />
 				</Setting>
 			</SettingsGroup>
 			<SettingsGroup label="Outline">
-				<Setting label="Colour" key="outline.colour" bind:value={outlineColourValue} isChild>
-					<input type="color" bind:value={outlineColourValue} class="w-9" />
+				<Setting label="Colour" isChild>
+					<input type="color" name="" id="" class="w-9" />
 				</Setting>
-				<Setting label="Thickness" key="outline.thickness" bind:value={outlineThicknessValue} isChild>
-					<input type="number" bind:value={outlineThicknessValue} min="0" max="5" class="w-9" />
+				<Setting label="Thickness" isChild>
+					<input type="number" name="" id="" value="0" min="0" max="5" class="w-9" />
 				</Setting>
 			</SettingsGroup>
 		</SettingsRow>
-		<Setting label="Message Type" key="message_type" bind:value={messageTypeValue}>
+		<Setting label="Message Type">
 			<RadioGroup
-				bind:selected={messageTypeValue}
+				bind:selected={messageType}
 				options={{
 					Default: "default",
 					First: "first",
@@ -82,15 +71,15 @@
 				}}
 			/>
 		</Setting>
-		<Setting label="Text Wrapping (force to Twitch's width)" key="wrap" bind:value={wrapValue}>
-			<input type="checkbox" bind:checked={wrapValue} />
+		<Setting label="Text Wrapping (force to Twitch's width)">
+			<input type="checkbox" name="" id="" />
 		</Setting>
 	</div>
 	<hr />
 	<div class="flex flex-col gap-2">
 		<div class="box relative h-62.5 w-full overflow-hidden">
 			<label class="btn absolute top-0 right-0 flex items-center gap-1.5 rounded-none! rounded-bl-xs! outline-zinc-800!">
-				<input type="checkbox" bind:checked={backgorundPreviewValue} />
+				<input type="checkbox" name="" id="" />
 				Background Preview
 			</label>
 			<div class="flex size-full items-center justify-center text-sm">
@@ -108,8 +97,8 @@
 
 <Modal bind:this={preferencesModal} class="flex flex-col gap-2">
 	<h1>Preferences</h1>
-	<Setting label="Always On Top" key="always_on_top" bind:value={alwaysOnTopValue} minimal class="between">
-		<input type="checkbox" bind:checked={alwaysOnTopValue} />
+	<Setting label="Always On Top" minimal class="between">
+		<input type="checkbox" name="" id="" />
 	</Setting>
 	<Setting label="Emote Cache" isChild>
 		<button class="btn">Reset</button>
--- src/lib/components/Setting.svelte
+++ src/lib/components/Setting.svelte
@@ -1,39 +1,22 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { settingsStorage } from "../stores/settingsStorage";
-
 	type Props = {
 		children: any;
 		label: string;
-		key?: string;
-		value?: any;
 		isChild?: boolean;
 		minimal?: boolean;
 		class?: string;
 	};
 
-	let { children, label, key, value = $bindable(), isChild = false, minimal = false, class: _class = "" }: Props = $props();
-
-	onMount(() => {
-		if (key != undefined && key in $settingsStorage) {
-			value = $settingsStorage[key];
-		}
-	});
-
-	$effect(() => {
-		if (key != undefined) {
-			$settingsStorage[key] = value;
-		}
-	});
+	let { children, label, isChild = false, minimal = false, class: _class = "" }: Props = $props();
 </script>
 
 {#if isChild}
-	<div class="{minimal ? '' : 'between'} {_class}">
+	<div class="{minimal ? "" : "between"} {_class}">
 		<span>{label}</span>
 		{@render children()}
 	</div>
 {:else}
-	<div class="{minimal ? '' : 'box between p-2'} {_class}">
+	<div class="{minimal ? "" : "box between p-2"} {_class}">
 		<h2>{label}</h2>
 		{@render children()}
 	</div>
