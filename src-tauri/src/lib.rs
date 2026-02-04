cdef090cffad09ea454de459e5646ac8b47cf7e4
Zane Shaw <zane@squidee.dev>
2026-02-04 11:38:28 +1100

    Revert "update bun and cargo dependencies"
    This reverts commit 7d738dc88ba3e2d9bde742b8ed1bd1841e10e545.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_fs::init())
