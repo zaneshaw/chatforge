eece96b2bd1515af52954bb4f81d0ecdb70bf712
Zane Shaw <zane@squidee.dev>
2026-02-04 11:38:28 +1100

    Revert "add help tooltip to outline setting"
    This reverts commit d1f51422165f54e327084923fcc13554aaa9e982.

--- src/lib/components/SettingsGroup.svelte
+++ src/lib/components/SettingsGroup.svelte
@@ -1,25 +1,13 @@
 <script lang="ts">
-	import { InfoIcon } from "@lucide/svelte";
-
 	type Props = {
 		children: any;
 		label: string;
-		help?: string;
 	};
 
-	let { children, label, help }: Props = $props();
+	let { children, label }: Props = $props();
 </script>
 
-<div class="box flex flex-col gap-1 p-2">
-	<div class="between">
-		<h2>{label}</h2>
-		{#if help}
-			<span title={help}>
-				<InfoIcon class="hover:stroke-twitch-text size-4 cursor-help stroke-zinc-400" />
-			</span>
-		{/if}
-	</div>
-	<div class="flex flex-col gap-1 *:ml-1.5">
-		{@render children()}
-	</div>
+<div class="box flex flex-col gap-1 p-2 [&>div]:ml-1.5">
+	<h2>{label}</h2>
+	{@render children()}
 </div>
